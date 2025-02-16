e6ab97589469c6ce67bcddcd1742c0b0af84750a
Annabelle Fait <imannaf8@gmail.com>
2025-02-16 11:31:31 -0500

    Task 3 - Service Fee Calculation

--- cc_8.js
+++ cc_8.js
@@ -17,3 +17,16 @@ function calculateDiscount(price, discountrate) { // Created a function that cal
 
 calculateDiscount(100, 0.2); // Calculates the discounted price of a product that costs $100 and has a 20% discount
 calculateDiscount(250, 0.15); // Calculates the discounted price of a product that costs $250 and has a 15% discount
+
+// Task 3 - Service Fee Calculation
+
+const calculateServiceFee = (amount, serviceType) => { // Created an arrow function that calculates a service fee based on the amount and the service type
+    let fee = 0
+    if (serviceType === "Premium") fee = amount * .15; // Applies a 15% fee if the service type is Premium
+    else if (serviceType ==="Standard") fee = amount * .10; // Applies a 10% fee if the service type is Standard
+    else if (serviceType === "Basic") fee = amount * .05; // Applies a 5% fee if the service type is Basic
+    console.log(`Service Fee $${(fee).toFixed(2)}`); // Logs the calculated service fee to two decimal places
+};
+
+calculateServiceFee(200, "Premium"); // Calculates the service fee of a $200 amount with Premium service
+calculateServiceFee(500, "Standard"); // Calculates the service fee of a $500 amount with Standard service
