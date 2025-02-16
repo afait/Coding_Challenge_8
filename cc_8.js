7145e24488c18429d0b67c25954830f165ee5a80
Annabelle Fait <imannaf8@gmail.com>
2025-02-16 11:19:09 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -7,3 +7,13 @@ function calculateSalary(baseSalary, bonus, taxRate) { // Created a function tha
 
 calculateSalary(5000, 500, 0.1); // Finds the employee salary given a base salary of 5000, a bonus of 500 and a taxrate of 10%
 calculateSalary(7000, 1000, 0.15); // Finds the employee salary given a base salary of 7000, a bonus of 1000 and a taxrate of 15%
+
+// Task 2 - Product Price After Discount
+
+function calculateDiscount(price, discountrate) { // Created a function that calculates discount price based on the price and discount rate
+    let FinalPrice = (price - (price *discountrate)).toFixed(2);
+    console.log(`Final Price: $${FinalPrice}`)
+} // Calculates the price by subtracting the calculated discount rate and rounds to two decimal places
+
+calculateDiscount(100, 0.2); // Calculates the discounted price of a product that costs $100 and has a 20% discount
+calculateDiscount(250, 0.15); // Calculates the discounted price of a product that costs $250 and has a 15% discount
